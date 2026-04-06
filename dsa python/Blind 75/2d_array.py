77ee2d423755bc844a354aff4c883763639bc676
JayPatelCode <pateljayparag@gmail.com>
2026-04-06 23:55:23 +0530

    2d array related code

--- dsa python/Blind 75/2d_array.py
+++ dsa python/Blind 75/2d_array.py
@@ -24,63 +24,63 @@
 
 
 
-####################PRINT upper triangle#####
-nums=[[5,20,3],[4,3,7],[2,1,4]]
-rows=len(nums)
-cols=len(nums[0])
-for i in range(rows):
-    for j in range(cols):
-        if j>=i:
-            print(nums[i][j],end=" ")
-        else:
-            print("*",end=" ")
-    print()
+# ###################PRINT upper triangle#####
+# nums=[[5,2,3],[4,3,7],[2,1,4]]
+# rows=len(nums)
+# cols=len(nums[0])
+# for i in range(rows):
+#     for j in range(cols):
+#         if j>=i:
+#             print(nums[i][j],end=" ")
+#         else:
+#             print("*",end=" ")
+#     print()
 
 
 ####################PRINT lower triangle#####
-nums=[[5,20,3],[4,3,7],[2,1,4]]
-rows=len(nums)
-cols=len(nums[0])
-for i in range(rows):
-    for j in range(cols):
-        if i>=j:
-            print(nums[i][j],end=" ")
-        else:
-            print("*",end=" ")
-    print()
+# nums=[[5,20,3],[4,3,7],[2,1,4]]
+# rows=len(nums)
+# cols=len(nums[0])
+# for i in range(rows):
+#     for j in range(cols):
+#         if i>=j:
+#             print(nums[i][j],end=" ")
+#         else:
+#             print("*",end=" ")
+#     print()
 
 
 
-####################PRINT diagonal#####
-nums=[[5,20,3],[4,3,7],[2,1,4]]
-rows=len(nums)
-cols=len(nums[0])
-for i in range(rows):
-    for j in range(cols):
-        if i==j:
-            print(nums[i][j],end=" ")
-        else:
-            print("*",end=" ")
-    print()
+# ####################PRINT diagonal#####
+# nums=[[5,20,3],[4,3,7],[2,1,4]]
+# rows=len(nums)
+# cols=len(nums[0])
+# for i in range(rows):
+#     for j in range(cols):
+#         if i==j:
+#             print(nums[i][j],end=" ")
+#         else:
+#             print("*",end=" ")
+#     print()
 
 
-####################PRINT diagonal#####
-nums=[[5,20,3],[4,3,7],[2,1,4]]
-rows=len(nums)
-n=len(nums)
-cols=len(nums[0])
-for i in range(rows):
-    for j in range(cols):
-        if (i+j==n-1):
-            print(nums[i][j],end=" ")
-        else:
-            print("*",end=" ")
-    print()
+# ####################PRINT diagonal#####
+# nums=[[5,20,3],[4,3,7],[2,1,4]]
+# rows=len(nums)
+# n=len(nums)
+# cols=len(nums[0])
+# for i in range(rows):
+#     for j in range(cols):
+#         if (i+j==n-1):
+#             print(nums[i][j],end=" ")
+#         else:
+#             print("*",end=" ")
+#     print()
 
 
 
 
-####################PRINT transpose#####
+# ####################PRINT transpose#####
 # nums=[[5,9,1],[2,3,7]]
 nums=[[1,2,3],[4,5,6],[7,8,9]]
 rows=len(nums)
@@ -90,6 +90,19 @@ result=[[0]*rows for _ in range(cols)]
 print(result)
 for i in range(rows):
     for j in range(cols):
-        result[j][i]=nums[i][j]
+        result[i][j]=nums[j][i]
 
 print(result)
+
+
+# nums=[[1,2,3],[4,5,6],[7,8,9]]
+# n=len(nums)
+# rows=n
+# cols=len(nums[0])
+# print(rows,cols)
+# result=[[0]*rows for i in range(cols)]
+# print(result)
+# for i in range(rows):
+#     for j in range(cols):
+#         result[j][i]=nums[i][j]
+# print(result)
