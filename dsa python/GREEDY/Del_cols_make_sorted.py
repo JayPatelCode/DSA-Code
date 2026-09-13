2f8244cd94337e530571f78218d5615c88d2ee62
JayPatelCode <pateljayparag@gmail.com>
2026-09-13 22:51:44 +0530

    WIP

---
+++ dsa python/GREEDY/Del_cols_make_sorted.py
@@ -0,0 +1,15 @@
+strs = ["abc", "bce", "cae"]
+
+class Solution:
+    def minDeletionSize(self, strs: list[str]) -> int:
+        n=len(strs)
+        k=len(strs[0])
+        count=0
+        for i in range(k):
+            for j in range(1,n):
+                if strs[j][i]<strs[j-1][i]:
+                    count+=1
+                    break
+        return count
+s=Solution()
+print(s.minDeletionSize(strs=strs))
