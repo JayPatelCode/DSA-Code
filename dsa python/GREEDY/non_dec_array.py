d8eb68d1cae4611c7610596bf4625febe5fbdcb8
JayPatelCode <pateljayparag@gmail.com>
2026-09-26 23:38:25 +0530

    Greedy Non dec array

---
+++ dsa python/GREEDY/non_dec_array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkPossibility(self, nums: list[int]) -> bool:
+        changes = 0
+
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                changes += 1
+
+                if changes > 1:
+                    return False
+
+                if i == 0 or nums[i - 1] <= nums[i + 1]:
+                    nums[i] = nums[i + 1]
+                else:
+                    nums[i + 1] = nums[i]
+
+        return True
